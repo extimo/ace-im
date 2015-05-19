97528c83a1d0a64f0fb30171eef6fa447faeb71d
Nemo <me@nemoge.com>
2015-05-19 19:41:29 +0800

    add time

--- public/assets/aim.js
+++ public/assets/aim.js
@@ -57,6 +57,10 @@ angular.module('AIMApp').controller('MessageCreatorCtrl', function($scope, socke
 		}
 		if($scope.newMessage.indexOf('/set') == 0){
 			var sps = $scope.newMessage.split(" ", 2);
+			if(sps[1].toUpperCase() == "SYSTEM"){
+				$scope.share.messages.push({content: 'resricted names', from: 'SYSTEM', createAt: new Date()});
+				return;
+			}
 			socket.emit('createMessage', 
 				{content: $scope.share.me + ' changes nick to ' + sps[1], from: 'SYSTEM', createAt: new Date()});
 			$scope.share.me = sps[1];
