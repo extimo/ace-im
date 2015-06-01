87bbb574cf3b9a3e46143fc3118b9b9b7cb96c0b
Nemo <me@nemoge.com>
2015-06-02 00:03:09 +0800

    emoji

--- public/assets/aim.js
+++ public/assets/aim.js
@@ -215,6 +215,7 @@ angular.module('AIMApp').directive('convertEmoji', function() {
 		restrict: 'AE',
 		template: '{{message.content}}',
 		link: function(scope, elem, attrs) {
+			alert(elem.html());
 			elem.html(emoji.replace_colons(elem.html()) + 'converted');
 		}
 	};
