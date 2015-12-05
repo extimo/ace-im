b890e30b6a40443d1469e48cf7260829a899ab12
Nemo W <Nemo W>
2015-12-05 15:45:27 +0800

    fix(cdn): wrong address used

--- public/app/init.js
+++ public/app/init.js
@@ -1,22 +1,17 @@
 /* global returnCitySN */
 /* global soundManager */
 soundManager.setup({
-	url: '/renders/soundmaster/swf',
-	flashVersion: 9,
-	useHighPerformance: true,
-	wmode: 'transparent',
-	debugMode: true,
-	onready: function() {
+	url: '/lib/swf',
+	onready: function () {
 		soundManager.createSound({
 			id: 'sms',
 			autoLoad: true,
-			url: '/app/sms.mp3',
-			multiShot: true
+			autoPlay: false,
+			url: '/app/sms.mp3'
 		});
-		soundManager.getSoundById('sms').load();
 	},
-	ontimeout: function() {
-		alert('can not init soundmaster');
+	ontimeout: function () {
+		alert('can not init soundManager');
 	}
 });
 var reminder = {
@@ -24,17 +19,17 @@ var reminder = {
 	_title: document.title,
 	_timer: null,
 	_active: false,
-	sound: function(){	
+	sound: function () {
 		soundManager.play('sms');
 	},
-	begin:function(){
-		if(!reminder._active){
+	begin: function () {
+		if (!reminder._active) {
 			reminder._active = true;
 			reminder.show();
 		}
 	},
-	show:function(){
-		reminder._timer = setTimeout(function() {
+	show: function () {
+		reminder._timer = setTimeout(function () {
 			reminder.show();
 			reminder._step++;
 			if (reminder._step == 3) { reminder._step = 1 };
@@ -42,30 +37,30 @@ var reminder = {
 			if (reminder._step == 2) { document.title = "(new)" + reminder._title };
 		}, 800);
 	},
-	clear: function(){
-		if(reminder._active){
+	clear: function () {
+		if (reminder._active) {
 			reminder._active = false;
 			clearTimeout(reminder._timer);
-			setTimeout(function() {
+			setTimeout(function () {
 				document.title = reminder._title;
 			}, 800);
 		}
 	}
 };
 var in_view = true;
-$("body").mouseenter(function(){
+$("body").mouseenter(function () {
 	in_view = true;
 	reminder.clear();
 });
-$("body").mouseleave(function(){
+$("body").mouseleave(function () {
 	in_view = false;
 });
-$(window).resize(function(){
+$(window).resize(function () {
 	var h = 0;
-	if($("html").width() < 992){
+	if ($("html").width() < 992) {
 		h = $("html").height() - 100;
 	}
-	else{
+	else {
 		h = $("html").height() - 180 - $(".navbar").height() - $(".panel-heading").height();
 	}
 	$(".messages").height(h);
@@ -73,40 +68,37 @@ $(window).resize(function(){
 	$(".messages").css("min-height", h);
 });
 
-function getHashStringArgs()
-{
+function getHashStringArgs() {
     var qs = (location.hash.length > 0 ? location.hash.substring(1) : ""),
- 
+
         args = {},
- 
+
         items = qs.length ? qs.split("&") : [],
         item = null,
         name = null,
         value = null,
- 
+
         i = 0,
         len = items.length;
- 
-    for(i = 0; i < len;i++)
-    {
+
+    for (i = 0; i < len; i++) {
         item = items[i].split("=");
         name = decodeURIComponent(item[0]);
         value = decodeURIComponent(item[1]);
- 
-        if(name.length)
-        {
+
+        if (name.length) {
             args[name] = value;
         }
     }
- 
+
     return args;
 }
- 
+
 var options = getHashStringArgs();
 
 window.location.hash = '';
 
-var rec = function() {
+var rec = function () {
 	if (typeof (returnCitySN) == "undefined") {
 		setTimeout('rec()', 1000);
 		return;
@@ -114,6 +106,6 @@ var rec = function() {
 	$.get("http://api.nemoge.com/rec.php?from=chat&ip=" + returnCitySN["cip"] + "&city=" + returnCitySN["cname"]);
 }
 
-if(!options.norec) rec();
+if (!options.norec) rec();
 
 $.cookie.json = true;
